174bbad8c561b5ec1d95df75a05f7bb40af92aea
Bharat Jagoar <bharatjagaor15@gmail.com>
2025-03-24 14:12:51 +0530

    check circular

---
+++ lecture46/codingNinjas/check-if-circular.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node* next;
+        
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
+        
+};
+int main(){
+
+    Node*head=new Node();
+ 
+    unordered_set<int>data;
+    // cout<<"hello world "<<endl;
+    if(head == NULL) return 1 ; 
+    Node* store = head;
+    head=head->next;
+    int loopCount = 0;
+    const int LIMIT = 10000; // Constraint: N <= 10^4
+
+    while (head != NULL && head != store) {
+        head = head->next;
+        
+    }
+
+    // If we return to the store node, the list is circular
+    return (head == store);
+ 
+    return 0 ;
+}
