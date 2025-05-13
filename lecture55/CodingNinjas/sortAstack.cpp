bc6f9c7a2ebf92479d77bfd46a02adfb4cbc24e1
Bharatjagoar <bharatjagoar15@gmail.com>
2025-05-13 09:50:36 +0530

    sort the stack

--- lecture55/CodingNinjas/sortAstack.cpp
+++ lecture55/CodingNinjas/sortAstack.cpp
@@ -8,13 +8,37 @@ stack<int> createStackFromArray(int arr[], int size) {
     for (int i = 0; i < size; ++i) {
         stk.push(arr[i]);
     }
-    // fdsa
+
     return stk;
 }
 
+void Pushdatain(stack<int>&data,int number){
+    if(data.empty()){
+        data.push(number);
+        return;
+    }
+    int top = data.top();
+    if(number>top){
+        data.push(number);
+        return;
+    }
+    data.pop();
+    Pushdatain(data,number);
+    data.push(top);
+}
+
+
+
 
 void sort(stack<int>&data){
-
+    
+    if(data.empty()){
+        return;
+    }
+    int top = data.top();
+    data.pop();
+    sort(data);
+    Pushdatain(data,top);
 }
 
 
@@ -23,13 +47,12 @@ int main(){
     int arr[] = {1,3,8,6,2,7}; // Example array
     int size = sizeof(arr) / sizeof(arr[0]);
     stack<int>input = createStackFromArray(arr, size);
-
-
+    cout<<"fdsa"<<endl;
+    sort(input);
     while (!input.empty()){
         cout<<"hello :: "<<input.top()<<endl;
         input.pop();
     }
-    
     // cout<<"hellow"<<endl;
     return 0 ;
 }
