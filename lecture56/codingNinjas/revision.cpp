d78af60221094964acf5e174532ba0484935a5e1
Bharatjagoar <bharatjagoar15@gmail.com>
2025-07-01 18:32:25 +0530

    Residual

---
+++ lecture56/codingNinjas/revision.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+    vector<int>arr = {2,1,4,3};
+    
+    for(int i = arr.size() - 1 ; i>=0 ; i--){
+        cout<<arr[i]<<endl;
+        
+    }
+ 
+    return 0 ;
+}
