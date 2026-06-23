63edc8b0cce683f4d6c5404be9a45ebf179b63e0
Bharat Jagoar <75258911+Bharatjagoar@users.noreply.github.com>
2026-06-23 10:41:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.09%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int find(int n) {
+        if (n <= 1)
+            return 1;
+
+        int a = 1, b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+    int climbStairs(int n) { return find(n); }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
