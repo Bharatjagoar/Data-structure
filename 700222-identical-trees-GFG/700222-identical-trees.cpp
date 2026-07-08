2b01d5810b6b0041202fa73600384d0cf994105f
Bharat Jagoar <75258911+Bharatjagoar@users.noreply.github.com>
2026-07-08 16:51:59 +0530

    Added solution - LeetHub

---
+++ 700222-identical-trees-GFG/700222-identical-trees.cpp
@@ -0,0 +1,38 @@
+/*
+class Node {
+	public:
+	int data;
+	Node* left;
+	Node* right;
+	
+	Node(int x) {
+		data = x;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+*/
+class Solution {
+	public:
+	bool isIdentical(Node* r1, Node* r2) {
+		// code here
+		if (r1 == nullptr && r2 != nullptr)
+			return false;
+		if (r2 == nullptr && r1 != nullptr)
+			return false;
+		if (r1 == nullptr && r2 == nullptr)
+			return true;
+			
+		if(r1->data != r2->data) return false;
+		bool left = isIdentical(r1->left, r2->left);
+		bool right = isIdentical(r1->right, r2->right);
+		
+		if(!(right&& left)) return false;
+		return true;
+	}
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
