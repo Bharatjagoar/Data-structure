b66c97bf6d12a9b1cfc430157d545a54281d8c82
Bharat Jagoar <75258911+Bharatjagoar@users.noreply.github.com>
2026-08-05 22:50:55 +0530

    Force committed solution - Time: O(H), Space: O(1)

---
+++ 700614-predecessor-and-successor-GFG/700614-predecessor-and-successor.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    Node* getpred(Node* root, int key) {
+        Node* current = root;
+        Node* ans = nullptr;
+        while (current) {
+            if (current->data < key) {
+                ans = current;
+                current = current->right;
+            } else {
+                current = current->left;
+            }
+        }
+        return ans;
+    }
+
+    Node* getsuc(Node* root, int key) {
+        Node* current = root;
+        Node* ans = nullptr;
+        while (current) {
+            if (current->data > key) {
+                ans = current;
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return ans;
+    }
+
+    vector<Node*> findPreSuc(Node* root, int key) {
+        Node* pred = getpred(root, key);
+        Node* succ = getsuc(root, key);
+        return {pred, succ};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
