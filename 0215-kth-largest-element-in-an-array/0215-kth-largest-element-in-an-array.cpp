d9da7bd8c89f10463be29aca74bfa8b859cd0cec
Bharat Jagoar <75258911+Bharatjagoar@users.noreply.github.com>
2026-09-15 23:22:32 +0530

    Time: 1216 ms (5%), Space: 69.1 MB (83.05%) - LeetHub - Time: O(n log n), Space: O(log n)

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void check(int index, int size, vector<int>& arr) {
+        int largestindex = index;
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        if (left < size && arr[largestindex] < arr[left]) {
+            largestindex = left;
+        }
+        if (right < size && arr[largestindex] < arr[right]) {
+            largestindex = right;
+        }
+
+        if (largestindex != index) {
+            swap(arr[largestindex], arr[index]);
+            check(largestindex, size, arr);
+        }
+    }
+    // we need to heapify the array
+    void heapify(vector<int>& arr) {
+        int size = arr.size();
+        int init = (size / 2) - 1;
+        for (int i = init; i >= 0; i--) {
+            check(i, size, arr);
+        }
+    }
+    void heapsort(vector<int>& arr) {
+        int size = arr.size();
+
+        for (int i = size - 1; i >= 0; i--) {
+            swap(arr[0], arr[i]);
+            check(0, i, arr);
+        }
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        heapify(nums);
+        heapsort(nums);
+        int size= nums.size();
+        cout << endl;
+        for (int i : nums) {
+            cout << i << "\t";
+        }
+        cout << endl;
+
+        return nums[size-k];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
